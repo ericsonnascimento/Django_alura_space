44ec43521ebb4eac063e7ae555c0e13ce65e2bfd
Ericson Nascimento <ericson@grupocyber.com.br>
2023-11-20 09:08:20 -0300

    Em galeria.models criamos o list_editable, em galeria.models importamos datetime e criamos um DateTimeField, em galeria.viwes mudamos a ordem de busca adicionado order_by(), configurado MEDIA_ROOT e MEDIA_URL para upload de arquivo para dentro do projeto (resta concluir a exibição no frontend)

--- galeria/models.py
+++ galeria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     OPCOES_CATEGORIA = [
@@ -12,8 +13,9 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='foto/%Y/%m/%d', blank=False)
     publicada = models.BooleanField(default=False)
+    data_publicacao = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
