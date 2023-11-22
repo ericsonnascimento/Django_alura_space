ca4eb25b842711f614f587954d2078432842053a
Ericson Nascimento <ericson@grupocyber.com.br>
2023-11-22 08:53:50 -0300

    Realizado ajustes no index para visualizar not_found.png, criado arquivo not_found.png dentro de static, ajustado __str__ dentro do models

--- galeria/models.py
+++ galeria/models.py
@@ -13,10 +13,10 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False, blank=False)
     categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.ImageField(upload_to='foto/%Y/%m/%d', blank=False)
+    foto = models.ImageField(upload_to='foto/%Y/%m/%d', null=True, blank=True)
     publicada = models.BooleanField(default=False)
     data_publicacao = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
 
