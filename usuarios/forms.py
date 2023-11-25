0fce99d0018eda65003bb6f2f2b20d26bd691aba
Ericson Nascimento <ericson@grupocyber.com.br>
2023-11-25 10:00:13 -0300

    Criado forms.py em usuarios e lançadas as classes LoginForm e CadastroForm, passada as classes do forms.py atravez do usuarios.views.py, refeito o frontend do login.html e cadastro.html para aceitar as classes adicionando a segurança do csrf_token

---
+++ usuarios/forms.py
@@ -0,0 +1,72 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva'
+            }
+        )
+    )
+
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        )
+    )
+
+class CadastroFroms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva'
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: joaosilva@gmail.com'
+            }
+        )
+    )
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: Digite sua senha'
+            }
+        )
+    )
+    repete_senha = forms.CharField(
+        label='Repita a Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente'
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroFroms
 
 # Create your views here.
 def login(request):
-    return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroFroms()
+    return render(request, 'usuarios/cadastro.html', {'form': form})
